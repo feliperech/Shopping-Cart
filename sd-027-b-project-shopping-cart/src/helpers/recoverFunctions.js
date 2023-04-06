54879dc140ce26a37869239cd2994d73945bc438
Felipe Gerber <felipegerber@outlook.com>
2023-04-06 02:25:51 -0300

    Finalização da função recoverFunctions

---
+++ sd-027-b-project-shopping-cart/src/helpers/recoverFunctions.js
@@ -0,0 +1,18 @@
+import { getSavedCartIDs } from './cartFunctions';
+import { createCartProductElement } from './shopFunctions';
+import { fetchProduct } from './fetchFunctions';
+
+const recoverFunction = () => {
+  const cardsSaved = getSavedCartIDs();
+  const cardElement = document.querySelector('.cart__products');
+
+  cardsSaved.forEach((element) => {
+    const fetchElement = fetchProduct(element);
+    Promise.all([fetchElement])
+      .then((response) => response.forEach((responseEl) => {
+        cardElement.appendChild(createCartProductElement(responseEl));
+      }));
+  });
+};
+
+export default recoverFunction;
