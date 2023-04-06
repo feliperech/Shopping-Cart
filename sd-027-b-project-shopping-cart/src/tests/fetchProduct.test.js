54ae63f5832ce79d6e5a3f6291e01eff357fc239
Felipe Gerber <felipegerber@outlook.com>
2023-04-06 02:22:18 -0300

    Finalização do teste da função fetchProduct

--- sd-027-b-project-shopping-cart/src/tests/fetchProduct.test.js
+++ sd-027-b-project-shopping-cart/src/tests/fetchProduct.test.js
@@ -1,10 +1,23 @@
 import './mocks/fetchSimulator';
 import { fetchProduct } from '../helpers/fetchFunctions';
 import product from './mocks/product';
+// import { it } from 'mocha';
 
 // implemente seus testes aqui
 describe('Teste a função fetchProduct', () => {
-  it('...', () => {
-    
+  it('Teste se a função fetchProduct é uma função', () => {
+    expect(typeof fetchProduct).toBe('function'); 
+  });
+
+  it('Teste se, ao chamar a função com argumento "MLB1405519561", retona o endpoint correto', async () =>{
+    const response = await fetchProduct('MLB1405519561');
+    expect(fetch).toBeCalledWith('https://api.mercadolibre.com/items/MLB1405519561');
+  });
+  it('Teste se, ao chamar a função com argumento MLB1405519561, retorna dados igual ao produto, importado no arquivo', async() =>{
+    const response = await fetchProduct('MLB1405519561');
+    expect(response).toEqual(product);
+  });
+  it('Teste se, ao chamar fetchProduct sem argumento, retorna o erro com "ID não informado', () => {
+    expect(fetchProduct()).rejects.toThrow(new Error('ID não informado'));
   });
 });
